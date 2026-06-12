a85714670620bdaebb724f7d294982e6c0926884
Francesca Yu <yui.francesca@gmail.com>
2026-06-12 17:33:07 +0200

    button.classList fixed

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ const lamp = document.querySelector('#lamp')
 
 button.addEventListener('click', () => {
     
+   if (button.classList.contains('spegni')) {
+        lamp.src = "assets/img/white_lamp.png";
+        button.textContent = "Accendi";
+        button.classList.remove('spegni');
+    } else {
+        lamp.src = "assets/img/yellow_lamp.png";
+        button.textContent = "Spegni";
+        button.classList.add('spegni');
+    }
+    });
+    
+
+    /*button.addEventListener('click', () => {
+    
     if (button.textContent.includes('Accendi')) {
         lamp.src="assets/img/yellow_lamp.png";
         button.textContent = "Spegni";
@@ -12,4 +26,4 @@ button.addEventListener('click', () => {
         button.textContent = "Accendi";
         button.classList.remove('spegni');
     }
-    });
+    });*/
