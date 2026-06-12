045ac8932153c4fbbac515c491efc48170111c45
Francesca Yu <yui.francesca@gmail.com>
2026-06-12 16:21:29 +0200

    hover fixed

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ button.addEventListener('click', () => {
     if (button.textContent.includes('Accendi')) {
         lamp.src="assets/img/yellow_lamp.png";
         button.textContent = "Spegni";
-        
+        button.classList.add('spegni');
     } else {
-         lamp.src="assets/img/white_lamp.png";
-         button.textContent = "Accendi";
+        lamp.src="assets/img/white_lamp.png";
+        button.textContent = "Accendi";
+        button.classList.remove('spegni');
     }
     });
