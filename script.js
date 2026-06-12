aca10a099f47825e010a35379c1dea7e997600a7
Francesca Yu <yui.francesca@gmail.com>
2026-06-12 16:08:06 +0200

    js set

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const button = document.getElementById('button');
-const lamp = document.getElementById('lamp')
+const button = document.querySelector('#button');
+const lamp = document.querySelector('#lamp')
 
 button.addEventListener('click', () => {
-
-});
+    
+    if (button.textContent.includes('Accendi')) {
+        lamp.src="assets/img/yellow_lamp.png";
+        button.textContent = "Spegni";
+        
+    } else {
+         lamp.src="assets/img/white_lamp.png";
+         button.textContent = "Accendi";
+    }
+    });
